550f82c34c612888eff9f7c022f5950063f96fca
akihiro-yamada1123 <paraisolullaby@gmail.com>
2020-05-13 12:24:11 +0900

    delete migrate

--- db/migrate/20200413046125_change_messages_table_2.rb
+++
@@ -1,9 +0,0 @@
-class ChangeMessagesTable2 < ActiveRecord::Migration[6.0]
-  def change
-      change_table :messages do |t|
-       t.rename :slack_id, :sns
-      end
-
-      add_column :slacks, :message_id, :integer
-  end
-end
